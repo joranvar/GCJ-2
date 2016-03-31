b272d3199ef85e126cdf07950639dc694a241afc
joranvar <bart.post@gmail.com>
2016-03-31 07:02:34 +0200

    Add missing type annotations

--- src/Test.hs
+++ src/Test.hs
@@ -6,8 +6,10 @@ import Solution (parse, examples, input, problems, output, Solution)
 showSolution :: Int -> Solution -> String
 showSolution i s = "Case #" ++ show i ++ ":" ++ show s
 
+tests :: Test
 tests = TestList
   [ "Can parse" ~: map (parse . tail . lines . input) examples ~?= map (map fst . problems) examples
   , "Can show"  ~: map (unlines . zipWith showSolution [1..] . map snd . problems) examples ~?= map output examples ]
 
+main :: IO ()
 main = void $ runTestTT tests
