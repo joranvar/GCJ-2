3376591cd43e78355f5a31d726079c192ea65226
joranvar <bart.post@gmail.com>
2016-04-01 21:02:21 +0200

    Rename A to P

--- src/GCJ/Y2008/Q/A.hs
+++ src/GCJ/Y2008/Q/A.hs
@@ -1,23 +1,23 @@
-module Solution (A(..), examples, input, problems, output, Solution, display, solve) where
+module Solution (P(..), examples, input, problems, output, Solution, display, solve) where
 import Data.List (delete)
 import GCJ (Problem(..))
 
 newtype SearchEngine = SearchEngine String deriving (Eq, Show)
 newtype Query        = Query String        deriving (Eq, Show)
 
-data A = A [SearchEngine] [Query]
+data P = P [SearchEngine] [Query]
   deriving (Eq, Show)
-instance GCJ.Problem A where
+instance GCJ.Problem P where
   parse [] = []
-  parse inp = A (map SearchEngine ss) (map Query qs) : parse rest
+  parse inp = P (map SearchEngine ss) (map Query qs) : parse rest
     where s:spart     = inp
           (ss, qpart) = splitAt (read s) spart
           q:qpart'    = qpart
           (qs, rest)  = splitAt (read q) qpart'
   parseExamples = [ ( "2\n5\nYeehaw\nNSM\nDont Ask\nB9\nGoogol\n10\nYeehaw\nYeehaw\nGoogol\nB9\nGoogol\nNSM\nB9\nNSM\nDont Ask\nGoogol\n5\nYeehaw\nNSM\nDont Ask\nB9\nGoogol\n7\nGoogol\nDont Ask\nNSM\nNSM\nYeehaw\nYeehaw\nGoogol\n"
-                    , [  A (map SearchEngine ["Yeehaw","NSM","Dont Ask","B9","Googol"])
-                           (map Query ["Yeehaw","Yeehaw","Googol","B9","Googol","NSM","B9","NSM","Dont Ask","Googol"])
-                      ,  A (map SearchEngine ["Yeehaw","NSM","Dont Ask","B9","Googol"])
+                    , [  P (map SearchEngine ["Yeehaw","NSM","Dont Ask","B9","Googol"])
+                            (map Query ["Yeehaw","Yeehaw","Googol","B9","Googol","NSM","B9","NSM","Dont Ask","Googol"])
+                      ,  P (map SearchEngine ["Yeehaw","NSM","Dont Ask","B9","Googol"])
                            (map Query ["Googol","Dont Ask","NSM","NSM","Yeehaw","Yeehaw","Googol"])])]
 
 data Solution = Solution Int
@@ -26,15 +26,15 @@ data Solution = Solution Int
 display :: Solution -> String
 display (Solution i) = " " ++ show i
 
-solve :: A -> Solution
-solve (A _ []) = Solution 0
-solve (A ss qs) = Solution $ result 0 ss qs
+solve :: P -> Solution
+solve (P _ []) = Solution 0
+solve (P ss qs) = Solution $ result 0 ss qs
   where result acc _ [] = acc
         result acc [] qs' = result (acc + 1) ss qs'
         result acc ss' (Query q:qs') = result acc (delete (SearchEngine q) ss') qs'
 
 data Example = Example { input    :: String
-                       , problems :: [(A, Solution)]
+                       , problems :: [(P, Solution)]
                        , output   :: String }
 
 examples :: [Example]
@@ -43,9 +43,9 @@ examples =
   , output = "Case #1: 1\n\
               \Case #2: 0\n\
               \"
-  , problems = [ ( A (map SearchEngine ["Yeehaw","NSM","Dont Ask","B9","Googol"])
-                     (map Query ["Yeehaw","Yeehaw","Googol","B9","Googol","NSM","B9","NSM","Dont Ask","Googol"])
+  , problems = [ ( P (map SearchEngine ["Yeehaw","NSM","Dont Ask","B9","Googol"])
+                      (map Query ["Yeehaw","Yeehaw","Googol","B9","Googol","NSM","B9","NSM","Dont Ask","Googol"])
                  , Solution 1 )
-               , ( A (map SearchEngine ["Yeehaw","NSM","Dont Ask","B9","Googol"])
+               , ( P (map SearchEngine ["Yeehaw","NSM","Dont Ask","B9","Googol"])
                      (map Query ["Googol","Dont Ask","NSM","NSM","Yeehaw","Yeehaw","Googol"])
                  , Solution 0 ) ] } ]
--- src/Test.hs
+++ src/Test.hs
@@ -1,6 +1,6 @@
 import Test.HUnit (runTestTT, Test(..), (~:), (~?=))
 import Control.Monad (void)
-import Solution (A(..), examples, input, problems, output, Solution, display, solve)
+import Solution (P(..), examples, input, problems, output, Solution, display, solve)
 import GCJ (Problem(..))
 
 showSolution :: Int -> Solution -> String
@@ -8,7 +8,7 @@ showSolution i s = "Case #" ++ show i ++ ":" ++ display s
 
 tests :: Test
 tests =
-  let parseExamples' = parseExamples :: [(String,[A])]
+  let parseExamples' = parseExamples :: [(String,[P])]
   in TestList [ "Can parse" ~: map (parse . tail . lines . fst) parseExamples' ~?= map snd parseExamples'
               , "Can show"  ~: map (unlines . zipWith showSolution [1..] . map snd . problems) examples ~?= map output examples
               , "Can solve" ~: map (map (solve . fst) . problems) examples ~?= map (map snd . problems) examples ]
